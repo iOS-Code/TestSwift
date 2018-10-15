9b4dbc514171c45146012f2ce01aa09495864518
岳琛 <yuechen@100tal.com>
2018-10-15 14:51:03 +0800

    no message

--- TestSwift/1.关于/1.1 A Swift Tour.swift
+++ TestSwift/1.关于/1.1 A Swift Tour.swift
@@ -7,3 +7,196 @@
 //
 
 import Foundation
+
+//TODO:简单值（Simple Values）
+
+func SimpleValues() {
+    
+    var myVar = 40
+    myVar = 50
+    let myConstant = 40
+    let myDouble:Double = 70
+    print(myVar, myConstant, myDouble)
+    
+    // 现式转换
+    let label = "The width is"
+    let width = 90
+    let widthLabel = label + " " + String(width)
+    print(widthLabel)
+    
+    // 快速转换
+    let apples = 3
+    let oranges = 5
+    let appleSummary = "I have \(apples) apples."
+    let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+    print(appleSummary, fruitSummary)
+    
+    // 创建数组和字典
+    var shoppingList = ["catfish", "water", "tulips", "blue paint"]
+    shoppingList[1] = "bottle of water";
+    
+    var occupations = [
+        "Malcolm": "Captain",
+        "Kaylee": "Mechanic",
+        ]
+    occupations["Jayne"] = "Public Relations"
+    
+    print(shoppingList, occupations)
+    
+    // 创建空数组和空字典
+    let emptyArray = [String]()
+    let emptyDictionary = [String: Float]()
+    print(emptyArray, emptyDictionary)
+    
+    shoppingList = []
+    occupations = [:]
+    print(shoppingList, occupations)
+}
+
+
+// TODO:控制流（Control Flow）
+
+func ControlFlow() {
+    
+    // IF的语句中，必须是一个布尔表达式，不会隐形与0进行对比
+    let individualScores = [75, 43, 103, 87, 12]
+    var teamScore = 0
+    for score in individualScores {
+        if score > 50 {
+            teamScore += 3
+        } else {
+            teamScore += 1
+        }
+    }
+    print(teamScore)
+    
+    // IF和Let来处理值缺失的情况，这些纸可以由可选值代表
+    // 可选值是一个具体的值或者nil，nil表示值缺失，在类型后面加一个❓来标记这个变量是可选的
+    let optionalString: String? = "Hello"
+    print(optionalString == nil)
+    
+    let optionalName: String? = "John Appleseed"
+    var greeting = "Hello!"
+    if let name = optionalName {
+        greeting = "Hello, \(name)"
+    }
+    print(greeting)
+    
+    // 如果变量的可选值是 nil，条件会判断为 false，大括号中的代码会被跳过。如果不是 nil，会将值解包并赋给  let 后面的常量，这样代码块中就可以使用这个值了。
+    let nickName: String? = nil
+    let fullName: String = "John Appleseed"
+    // 另一种处理可选值的方法是通过使用 ?? 操作符来提供一个默认值。如果可选值缺失的话，可以使用默认值来代替。
+    let informalGreeting = "Hi \(nickName ?? fullName)"
+    print(informalGreeting)
+    
+    
+    // Switch支持任意类型数据及各种比较操作
+    // 注意 let 在Switch的条件等式中，它将匹配等式的值赋给常量 x
+    // 运行Switch中匹配到的条件后，程序会退出Switch语句
+    
+    let vegetable = "red pepper"
+    switch vegetable {
+    case "celery":
+        print("Add some raisins and make ants on a log.")
+    case "cucumber", "watercress":
+        print("That would make a good tea sandwich.")
+    case let x where x.hasSuffix("pepper"):
+        print("Is it a spicy \(x)?")
+    default:
+        print("Everything tastes good in soup.")
+    }
+    
+    // 遍历
+    // 使用 for-in 遍历字典，需要两个变量表示键值
+    let interestingNumbers = [
+        "Prime":[12, 13, 14, 15, 17, 19],
+        "Fibonacci":[20, 21, 22, 26, 28, 29],
+        "Square":[31, 34, 36, 37, 38, 39]
+    ]
+    
+    var largest = 0
+    
+    for (kind, numbers) in interestingNumbers {
+        print(kind)
+        for number in numbers {
+            largest = number > largest ? number : largest;
+            print("number is \(number), largest is \(largest)")
+        }
+    }
+    
+    print(largest)
+    
+    
+    // 重复
+    // 使用While
+    var n = 2
+    while n < 100 {
+        n = n * 2
+    }
+    print(n)
+    
+    // 使用repeat
+    var m = 2
+    repeat {
+        m = m * 2
+    } while m < 100
+    
+    print(m)
+    
+    // 使用范围0到3
+    var total1 = 0
+    for i in 0..<4 {
+        total1 += i
+    }
+    print(total1)
+    
+    // 使用范围0到4
+    var total2 = 0
+    for i in 0...4 {
+        total2 += i
+    }
+    print(total2)
+
+}
+
+
+//TODO:函数和闭包（Functions and Closures）
+
+func FunctionsClosures() {
+    
+}
+
+//TODO:对象和类（Objects and Classes）
+
+func ObjectsClasses() {
+    
+}
+
+
+//TODO:枚举和结构体（Enumerations and Structures）
+
+func EnumerationsStructures() {
+    
+}
+
+
+//TODO:协议和扩展（Protocols and Extensions）
+
+func ProtocolsExtensions() {
+    
+}
+
+
+//TODO:错误处理（Error Handling）
+
+func ErrorHandling() {
+    
+}
+
+
+//TODO:泛型（Generics）
+
+func Generics() {
+    
+}
+
